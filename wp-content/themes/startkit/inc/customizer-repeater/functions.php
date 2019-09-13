5206dc4e1cc857fbe0f246eb6a81285bebe7386b
penajpablo <54922281+penajpablo@users.noreply.github.com>
2019-09-12 21:21:39 -0300

    Revert "prueba12/9 14.27"
    This reverts commit ab0f2c56c04ec2e9f6faf49efab6b0e1f5b1218f.

--- wp-content/themes/startkit/inc/customizer-repeater/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-define( 'STARTKIT_CUSTOMIZER_REPEATER_VERSION', '1.1.0' );
-
-require get_template_directory() . '/customizer-repeater/inc/customizer.php';
--- wp-content/themes/startkit/sidebar.php
+++
@@ -1,4 +0,0 @@
-<?php 
-if( is_active_sidebar('sidebar-primary') ) : 
-	dynamic_sidebar('sidebar-primary'); 
-endif; ?>
