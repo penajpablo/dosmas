4f6714471129e82ee3d938360c69516a26aba650
penajpablo <54922281+penajpablo@users.noreply.github.com>
2019-09-13 12:53:07 -0300

    primi

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; ClasesBateriaPinar &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='./wp-includes/css/dashicons.min.css?ver=5.2.2' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='./wp-includes/css/buttons.min.css?ver=5.2.2' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='./wp-admin/css/forms.min.css?ver=5.2.2' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='./wp-admin/css/l10n.min.css?ver=5.2.2' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='./wp-admin/css/login.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://clasesbateria-pinar.cf/wp-includes/css/dashicons.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://clasesbateria-pinar.cf/wp-includes/css/buttons.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://clasesbateria-pinar.cf/wp-admin/css/forms.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://clasesbateria-pinar.cf/wp-admin/css/l10n.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://clasesbateria-pinar.cf/wp-admin/css/login.min.css?ver=5.2.2' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -22,7 +22,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-	<form name="loginform" id="loginform" action="./index.html" method="post">
+	<form name="loginform" id="loginform" action="https://clasesbateria-pinar.cf/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -34,13 +34,13 @@
 			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-				<input type="hidden" name="redirect_to" value="./wp-admin/" />
+				<input type="hidden" name="redirect_to" value="https://clasesbateria-pinar.cf/wp-admin/" />
 					<input type="hidden" name="testcookie" value="1" />
 	</p>
 	</form>
 
 			<p id="nav">
-					<a href="./index.html?action=lostpassword">Lost your password?</a>
+					<a href="https://clasesbateria-pinar.cf/wp-login.php?action=lostpassword">Lost your password?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -57,15 +57,12 @@
 			if(typeof wpOnload=='function')wpOnload();
 			</script>
 
-			<p id="backtoblog"><a href="./index.html">
+			<p id="backtoblog"><a href="https://clasesbateria-pinar.cf/">
 		&larr; Back to ClasesBateriaPinar	</a></p>
 			
 	</div>
 
 	
-		<script>
-	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
-	</script>
 		<div class="clear"></div>
 	</body>
 	</html>
