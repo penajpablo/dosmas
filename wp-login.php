4271ec82bb6fca073eb9de06bea79320958bc170
penajpablo <54922281+penajpablo@users.noreply.github.com>
2019-09-20 19:55:45 -0300

    20.09  19.55

--- wp-login.php
+++ wp-login.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 	<!--[if IE 8]>
-		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-GB">
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="es">
 	<![endif]-->
 	<!--[if !(IE 8) ]><!-->
-		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB">
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
 	<!--<![endif]-->
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Log In &lsaquo; ClasesBateriaPinar &#8212; WordPress</title>
+	<title>Acceder &lsaquo; ClasesBateriaPinar &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
 <link rel='stylesheet' id='dashicons-css'  href='https://clasesbateria-pinar.cf/wp-includes/css/dashicons.min.css?ver=5.2.2' type='text/css' media='all' />
 <link rel='stylesheet' id='buttons-css'  href='https://clasesbateria-pinar.cf/wp-includes/css/buttons.min.css?ver=5.2.2' type='text/css' media='all' />
@@ -17,30 +17,34 @@
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
-		</head>
-	<body class="login login-action-login wp-core-ui  locale-en-gb">
+	<link rel="icon" href="https://clasesbateria-pinar.cf/wp-content/uploads/2019/09/cropped-151730-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://clasesbateria-pinar.cf/wp-content/uploads/2019/09/cropped-151730-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://clasesbateria-pinar.cf/wp-content/uploads/2019/09/cropped-151730-180x180.png" />
+<meta name="msapplication-TileImage" content="https://clasesbateria-pinar.cf/wp-content/uploads/2019/09/cropped-151730-270x270.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-es-es">
 		<div id="login">
-		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+		<h1><a href="https://es.wordpress.org/">Funciona gracias a WordPress</a></h1>
 	
 	<form name="loginform" id="loginform" action="https://clasesbateria-pinar.cf/wp-login.php" method="post">
 	<p>
-		<label for="user_login">Username or Email Address<br />
+		<label for="user_login">Nombre de usuario o dirección de correo<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
 	</p>
 	<p>
-		<label for="user_pass">Password<br />
+		<label for="user_pass">Contraseña<br />
 		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
 	</p>
-			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Recuérdame</label></p>
 	<p class="submit">
-		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acceder" />
 				<input type="hidden" name="redirect_to" value="https://clasesbateria-pinar.cf/wp-admin/" />
 					<input type="hidden" name="testcookie" value="1" />
 	</p>
 	</form>
 
 			<p id="nav">
-					<a href="https://clasesbateria-pinar.cf/wp-login.php?action=lostpassword">Lost your password?</a>
+					<a href="https://clasesbateria-pinar.cf/wp-login.php?action=lostpassword">¿Olvidaste tu contraseña?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -58,12 +62,15 @@
 			</script>
 
 			<p id="backtoblog"><a href="https://clasesbateria-pinar.cf/">
-		&larr; Back to ClasesBateriaPinar	</a></p>
+		&larr; Volver a ClasesBateriaPinar	</a></p>
 			
 	</div>
 
 	
-		<div class="clear"></div>
+			<script type="text/javascript">
+			var wc_product_block_data = JSON.parse( decodeURIComponent( '%7B%22min_columns%22%3A1%2C%22max_columns%22%3A6%2C%22default_columns%22%3A3%2C%22min_rows%22%3A1%2C%22max_rows%22%3A6%2C%22default_rows%22%3A1%2C%22thumbnail_size%22%3A230%2C%22placeholderImgSrc%22%3A%22http%3A%5C%2F%5C%2Flocalhost%5C%2Fdosmas%5C%2Fwp-content%5C%2Fuploads%5C%2Fwoocommerce-placeholder-230x350.png%22%2C%22min_height%22%3A500%2C%22default_height%22%3A500%2C%22isLargeCatalog%22%3Afalse%2C%22limitTags%22%3Afalse%2C%22hasTags%22%3Afalse%2C%22productCategories%22%3A%5B%7B%22term_id%22%3A16%2C%22name%22%3A%22Uncategorized%22%2C%22slug%22%3A%22uncategorized%22%2C%22term_group%22%3A0%2C%22term_taxonomy_id%22%3A16%2C%22taxonomy%22%3A%22product_cat%22%2C%22description%22%3A%22%22%2C%22parent%22%3A0%2C%22count%22%3A0%2C%22filter%22%3A%22raw%22%2C%22link%22%3A%22http%3A%5C%2F%5C%2Flocalhost%5C%2Fdosmas%5C%2Fproduct-category%5C%2Funcategorized%5C%2F%22%7D%5D%2C%22homeUrl%22%3A%22http%3A%5C%2F%5C%2Flocalhost%5C%2Fdosmas%5C%2F%22%7D' ) );
+		</script>
+			<div class="clear"></div>
 	</body>
 	</html>
 	
